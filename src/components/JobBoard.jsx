99792b310845783fe250ce6200169a5a9d722fa8
ImashaKalpani <kalpaniimasha76@gmail.com>
2025-06-15 21:54:14 +0530

    new

--- src/components/JobBoard.jsx
+++ src/components/JobBoard.jsx
@@ -93,24 +93,24 @@ const JobBoard = () => {
             {/* Filter Buttons */}
             <div className="flex gap-3 p-3 flex-wrap pr-4">
               <button className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-[#e7eef3] pl-4 pr-2">
-                <p className="text-[#4c799a] text-sm font-medium leading-normal">Location</p>
-                <div className="text-[#4c799a]">
+                <p className="text-[#white] text-sm font-medium leading-normal">Location</p>
+                <div className="text-[#white]">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
                     <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
                   </svg>
                 </div>
               </button>
               <button className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-[#e7eef3] pl-4 pr-2">
-                <p className="text-[#4c799a] text-sm font-medium leading-normal">Industry</p>
-                <div className="text-[#4c799a]">
+                <p className="text-[#white] text-sm font-medium leading-normal">Industry</p>
+                <div className="text-[#white]">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
                     <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
                   </svg>
                 </div>
               </button>
               <button className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-[#e7eef3] pl-4 pr-2">
-                <p className="text-[#4c799a] text-sm font-medium leading-normal">Skills</p>
-                <div className="text-[#4c799a]">
+                <p className="text-[#white] text-sm font-medium leading-normal">Skills</p>
+                <div className="text-[#white]">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
                     <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
                   </svg>
